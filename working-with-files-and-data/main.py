269974a111ad7824c2472f39fd4c57bea2ba64a5
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-11-06 13:00:01 +0000

    Add working with files: using with

--- working-with-files-and-data/main.py
+++ working-with-files-and-data/main.py
@@ -1,25 +1,9 @@
-# f = open("./config.txt")
+with open("./config.txt", "rt") as file:
+    content = file.read()
+    print(content)
 
-# .  => current working directory
-# .. => parent directory
-# f = open("./config.txt", "rt")
-# content = f.read(5)
-# print(content)
+    print(file.closed)
+    file.seek(0)
+    print(file.read())
 
-# content = f.read(3)
-# print(content)
-
-# print(f.tell())
-
-# f.seek(2)
-# print(f.read(2))
-
-# f.seek(0)
-# print(f.read(5))
-f = open("./config.txt")
-print(f.read())
-
-# do stuff
-print("#" * 20)
-f.seek(0)
-print(f.read())
+print(f"closed? {file.closed}")
