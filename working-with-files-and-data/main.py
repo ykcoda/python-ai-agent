0ef0fd9d0114ee1648ed1665f0d3464a6e1c6fb0
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-11-06 20:40:27 +0000

    Add how to manage json data

--- working-with-files-and-data/main.py
+++ working-with-files-and-data/main.py
@@ -1,13 +1,37 @@
+import httpx
 import json
-from pprint import pprint
-
-with open("./sample.json", "r") as f:
-    config = json.load(f)
-
-print(config)
-
-pretty_json = json.dumps(config, indent=4)
-print(pretty_json)
 
 
-print(config["model"])
+url = "https://jsonplaceholder.typicode.com/posts"
+
+try:
+    response = httpx.get(url)
+    response.raise_for_status()
+    post_data = json.loads(response.text)
+    print(post_data[0]["title"])
+
+    for item in post_data:
+        print(item)
+
+    print(post_data[0]["title"])
+
+
+except httpx.RequestError as e:
+    print(f"{e}")
+except httpx.HTTPError as e:
+    print(f"{e}")
+
+
+response1 = {
+    "usage": {
+        "total_tokens": 1000,
+        "details": {"prompt_tokens": 300, "completion_tokens": 700},
+    },
+    "choices": [
+        {"text": "The capital of France is Paris", "index": 0},
+        {"text": "France capital is Paris", "index": 1},
+    ],
+}
+
+choice_text = [choice for choice in response1["choices"]]
+print(choice_text)
