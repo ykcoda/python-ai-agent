41dd6fe54fa3426e12b599073772730341c4d437
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-11-06 20:11:10 +0000

    Add Write CSV File

--- working-with-files-and-data/main.py
+++ working-with-files-and-data/main.py
@@ -1,12 +1,23 @@
-# READING CSV FILES IN PYTHON
-
 import csv
 
-with open("./model_logs.csv", "r") as csv_f:
-    reader = csv.reader(csv_f)
-    next(reader)
 
-    token_data = {row[0]: int(row[3]) for row in reader}
-    peak_day = max(token_data, key=token_data.get)
+with open("sample.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["Name", "Age", "Grade"])
 
-    print(peak_day)
+    models = [
+        ("Kwame", 23, "A"),
+        ("Kofi", 12, "B"),
+        ("Kwame", 76, "C"),
+        ("Kwame", 53, "A"),
+    ]
+
+    for model in models:
+        writer.writerow(model)
+
+
+data = [["Name", "Age"], ["ama", 12], ["kofi", 11]]
+with open("tab_logs.csv", "w") as csv_file:
+    writer = csv.writer(csv_file)
+
+    writer.writerows(data)
