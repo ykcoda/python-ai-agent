6802c33fd6508283246f4243037fb8920d449441
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-11-06 13:14:29 +0000

    Add working with files: reading and iterating over text files

--- working-with-files-and-data/main.py
+++ working-with-files-and-data/main.py
@@ -1,9 +1,27 @@
+# Reading file into list
+
+# example 1
+my_list = []
+
 with open("./config.txt", "rt") as file:
-    content = file.read()
+    content = file.read().splitlines()
+    for c in content:
+        my_list.append(c)
+
+
+# example 2
+
+with open("./config.txt", "rt") as file:
+    content = file.readlines()
+    content[0] = "PORT=7777"
+    print(content[0])
+
+# example 3
+with open("./config.txt", "rt") as file:
+    content = list(file)
     print(content)
 
-    print(file.closed)
-    file.seek(0)
-    print(file.read())
-
-print(f"closed? {file.closed}")
+# Iterate over a file
+with open("./config.txt", "rt") as file:
+    for f in file:
+        print(f, end=" ")
