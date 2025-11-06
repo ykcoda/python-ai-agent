a76229dac46581a3a03550f49ecadfc6cf2deb18
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-11-06 13:49:56 +0000

    Add File handling project

--- working-with-files-and-data/main.py
+++ working-with-files-and-data/main.py
@@ -1,22 +1,10 @@
-# Writing to a text file
-with open("./config.txt", "wt") as file:
-    file.write("Just a line.\n")
-    file.write("Just a second line")
+with open("./devices.txt", "rt") as file:
+    content = file.read().splitlines()
+    device_list = list()
+    for c in content[1:]:
+        device_list.append(c.split(":"))
 
-
-with open("./config.txt", "wt") as file:
-    file.write("Just a line.\nJust a second line")
-
-with open("./config1.txt", "at") as file:
-    file.write("This is a new line.\n")
-    file.write("This is a new line.\n")
-
-
-with open("./config.txt", "r+") as file:
-    file.write("Line as the beginning\n")
-
-with open("./config.txt", "r+") as file:
-    file.seek(10)
-    file.write("Line as the beginning\n")
-    file.seek(15)
-    print(file.read())
+    print(device_list)
+    print("#" * 80)
+    for device in device_list:
+        print(f"pinging.... {device[1]}")
