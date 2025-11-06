1da6c833699b2c4c70105442e52c4952b2e4860d
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-11-06 20:18:17 +0000

    Add how to load json data

--- working-with-files-and-data/main.py
+++ working-with-files-and-data/main.py
@@ -1,23 +1,13 @@
-import csv
+import json
+from pprint import pprint
+
+with open("./sample.json", "r") as f:
+    config = json.load(f)
+
+print(config)
+
+pretty_json = json.dumps(config, indent=4)
+print(pretty_json)
 
 
-with open("sample.csv", "w", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow(["Name", "Age", "Grade"])
-
-    models = [
-        ("Kwame", 23, "A"),
-        ("Kofi", 12, "B"),
-        ("Kwame", 76, "C"),
-        ("Kwame", 53, "A"),
-    ]
-
-    for model in models:
-        writer.writerow(model)
-
-
-data = [["Name", "Age"], ["ama", 12], ["kofi", 11]]
-with open("tab_logs.csv", "w") as csv_file:
-    writer = csv.writer(csv_file)
-
-    writer.writerows(data)
+print(config["model"])
