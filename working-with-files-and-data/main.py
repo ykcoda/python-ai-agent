fd512dbb1487ec7b67deb289c37f8d26862ab711
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-11-06 17:39:56 +0000

    Add Project: Challenge#10

--- working-with-files-and-data/main.py
+++ working-with-files-and-data/main.py
@@ -1,10 +1,14 @@
 with open("./sample1.txt") as file:
     content = file.read().split()
-    print(content)
 
     my_dict = {}
 
     for c in content:
         my_dict[c] = len(c)
 
-    print(my_dict)
+    # s_my_dict = sorted(my_dict.items(), key=lambda x: x[1], reverse=True)
+
+    nn = sorted(my_dict.items())
+    ss = sorted(my_dict.items(), key=lambda x: x[1], reverse=True)
+
+    print(ss[:100])
