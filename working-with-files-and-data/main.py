ac71ce1262251635fb1780540b9823b949976bad
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-11-06 15:42:15 +0000

    Add Project: Challenge#6

--- working-with-files-and-data/main.py
+++ working-with-files-and-data/main.py
@@ -1,15 +1,40 @@
-import time
+def get_file_details(f):
+    with open(f) as file:
+        c1 = file.read().splitlines()
+        print(c1)
+        print(f"Total lines: {len(c1)}")
+        print("#" * 50)
+
+        words = 0
+        for l in list(c1):
+            words += len(l.split())
+
+        print(f"Total Words: {words}")
+
+        print("#" * 50)
+        s1 = "".join(c1)
+        print(f"Total Charaters: {len(list(s1))}")
 
 
-def tail(filename, n=0):
-    with open(filename, "rt") as file:
-        content = file.readlines()
-
-        for c in content[-n:]:
-            print(c)
+ff = get_file_details("./sample.txt")
 
 
-while True:
-    time.sleep(3)
-    content = tail("./sample.txt", 5)
-    print("#" * 50)
+def wc(file):
+    with open(file, "r") as f:
+        # reading the file into a list
+        content = f.read().splitlines()
+
+        lines = len(content)
+
+        words = 0
+        for line in content:
+            words += len(line.split())
+
+        chars = 0
+        for line in content:
+            chars += len(list(line))
+
+        return lines, words, chars
+
+
+print(wc("./sample.txt"))
