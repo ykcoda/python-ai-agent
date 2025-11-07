b47170c74e360d2400622dbe8c9edba520e6f5dd
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-11-07 21:37:51 +0000

    Illustrate how to use OpenAI client:v1

--- llm_apis_genai/main_openai.py
+++ llm_apis_genai/main_openai.py
@@ -3,7 +3,24 @@ from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
+os.getenv("OPENAI_API_KEY")
 
-api_key = os.getenv("API_KEY")
 
-client = OpenAI(api_key=api_key)
+question = input("Ask any question: ")
+
+client = OpenAI()
+
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "developer", "content": "You a python teacher and programmer"},
+        {"role": "user", "content": question},
+    ],
+    temperature=0.7,
+)
+
+print(response.choices[0].message.content)
+
+
+# for model in client.models.list():
+#     print(model.id)
