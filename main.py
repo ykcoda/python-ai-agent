619f0bfb2e3ccc0d7ce0b12506e751967cb0d168
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-11-07 00:06:15 +0000

    Add Porject: Checking network connectivity with PING

--- main.py
+++ main.py
@@ -1,40 +1,15 @@
-# Project: Making an API Request to OpenAPI
-import os
-import json
-import httpx
-from dotenv import load_dotenv
-
-load_dotenv()
-api_key = os.getenv("API_KEY")
-
-url = "https://api.openai.com/v1/chat/completions"
-
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {api_key}",
-}
-
-data = {
-    "model": "gpt-4o",
-    "messages": [
-        {"role": "system", "content": "You are a helpful assistant."},
-        {
-            "role": "user",
-            "content": "Give me the most interesting question you have ever been asked.",
-        },
-    ],
-    "temperature": 0.7,
-}
+import subprocess
 
 
-response = httpx.post(url=url, headers=headers, json=data)
-if response.status_code == 200:
-    result = response.json()
-    pretty_json = json.dumps(result, indent=4)
-    print(pretty_json)
-    print("\n")
-    reply = result["choices"][0]["message"]["content"]
+with open("hosts.txt") as file:
+    ips = file.read().splitlines()
 
-    print(reply)
-else:
-    print(response.status_code, response.text)
+    for ip in ips:
+        try:
+            command = f"ping -c 5 {ip}"
+            output = subprocess.check_output(command.split())
+            print(output.decode())
+        except subprocess.CalledProcessError as e:
+            print(f"{e}")
+
+        print("#" * 50)
