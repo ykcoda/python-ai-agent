a303cb3419b6f31de31a0413f7a0185ebd43cf0c
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-11-07 15:12:30 +0000

    Illustrate how to use httpx: added a post_request function

--- httpxx/main.py
+++ httpxx/main.py
@@ -2,11 +2,17 @@ import httpx
 import asyncio
 import logging
 import json
-from typing import Dict, Any
+import os
+from dotenv import load_dotenv
 
 ######################################################################################################
+load_dotenv()
+api_key = os.getenv("API_KEY")
+######################################################################################################
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    filename="logs.txt",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
 )
 ######################################################################################################
 
@@ -14,17 +20,26 @@ logging.basicConfig(
 class HttpService:
     """HttpService will accept a URL and make an API call."""
 
-    def __init__(self, url, timeout=5):
+    def __init__(
+        self,
+        url: str,
+        headers: dict | None = None,
+        data: dict | None = None,
+        timeout: int = 5,
+    ):
         """Accepts url and timeout"""
         self.url = url
+        self.headers = headers
+        self.data = data
         self.timeout = timeout
 
-    @property
-    async def make_http_request(self):
-        """Make an asynchronous http request and returns a json"""
+    async def get_request(self):
+        """Make an asynchronous http get request and return: JSON"""
         async with httpx.AsyncClient() as client:
             try:
-                response = await client.get(url=self.url, timeout=self.timeout)
+                response = await client.get(
+                    url=self.url, headers=self.headers, timeout=self.timeout
+                )
                 response.raise_for_status()
                 return json.dumps(response.json(), indent=4)
             except httpx.RequestError as e:
@@ -35,10 +50,57 @@ class HttpService:
                 logging.error(f"{e}")
             return None
 
+    async def post_request(self):
+
+        async with httpx.AsyncClient() as client:
+            try:
+                response = await client.post(
+                    url=self.url,
+                    headers=self.headers,
+                    json=self.data,
+                    timeout=self.timeout,
+                )
+                response.raise_for_status()
+                return response.json()
+            except httpx.RequestError as e:
+                logging.error(f"{e}")
+            except httpx.HTTPError as e:
+                logging.error(f"{e}")
+            except Exception as e:
+                logging.error(f"{e}")
+            return None
+
 
 if __name__ == "__main__":
-    url = "https://api.open-meteo.com/v1/fore?latitude=35&longitude=139&hourly=temperature_2m"
-    http_client = HttpService(url)
-    data = asyncio.run(http_client.make_http_request)
-    if data:
-        print(data)
+
+    while True:
+        try:
+            question = input("Ask anything: ")
+
+            url = "https://api.openai.com/v1/chat/completions"
+
+            headers = {
+                "Authorization": f"Bearer {api_key}",
+                "Content-Type": "application/json",
+            }
+
+            data = {
+                "model": "gpt-4o-mini",
+                "messages": [
+                    {"role": "system", "content": "You are a heplful assistant."},
+                    {"role": "user", "content": question},
+                ],
+                "temperature": 0.7,
+            }
+
+            http_client = HttpService(url=url, headers=headers, data=data, timeout=20)
+            data = asyncio.run(http_client.post_request())
+
+            if data:
+                print(data["choices"][0]["message"]["content"])  # type: ignore
+                print()
+                print("#" * 110)
+        except Exception as e:
+            logging.error(f"{e}")
+        except KeyboardInterrupt as e:
+            logging.warning(f"You existed the app....! {e}")
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import json
 url = "https://api.open-meteo.com/v1/forecast?latitude=35&longitude=139&hourly=temperature_2m"
 client = HttpService(url=url, timeout=2)
 
-data = asyncio.run(client.make_http_request)
+data = asyncio.run(client.get_request())
 if data:
     json_data = json.loads(data)
     print(json_data["hourly"])
