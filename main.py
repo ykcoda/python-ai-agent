80c486f32cf8007c446bf685432cada001e621fe
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-11-07 06:43:03 +0000

    Add OOP: Instance and class attributes

--- main.py
+++ main.py
@@ -4,10 +4,28 @@
 class Robot:
     """This class implements a Robot"""
 
+    population = 0  # a class attribute
+
     def __init__(self, name: str, year: int):
         self.name = name
         self.year = year
+        Robot.population += 1
+
+    def setEnegy(self, energy):
+        self.energy = energy
 
 
 r1 = Robot("Robot1", 2025)
+r2 = Robot("Robot2", 2050)
 print(f"Robot name: {r1.name}")
+r1.setEnegy(500)
+print(r1.energy)
+
+print(getattr(r1, "energy"))
+print(r1.__dict__)
+
+# print(r1.brand)
+print(getattr(r1, "brand", "N/A"))
+
+
+print(f"Robots alive: {Robot.population}")
